2882916405826393363e4a48328cb8ca3e8de993
Bram Musuko Panjaitan <bram.musuko@gmail.com>
2020-08-01 16:34:11 +0700

    hello world

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { ApolloServer } from "apollo-server-express";
+import * as Express from "express";
+import { buildSchema, Resolver, Query } from "type-graphql";
+import "reflect-metadata";
+
+@Resolver()
+class HelloResolver {
+  @Query(() => String)
+  async hello() {
+    return "Hello World!";
+  }
+}
+
+const main = async () => {
+  const schema = await buildSchema({
+    resolvers: [HelloResolver],
+  });
+  const apolloServer = new ApolloServer({ schema });
+  const app = Express();
+  apolloServer.applyMiddleware({ app });
+  app.listen(4000, () => {
+    console.log("server has started at port 4000");
+  });
+};
+
+main();
